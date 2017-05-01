77ce2bcbd50c7c04bb7c21d58ccc255a4a054d61
Jamie Burnip <jamieburnip@gmail.com>
2017-05-01 23:27:06 +0100

    Install fractal

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
          */
         Laravel\Tinker\TinkerServiceProvider::class,
         Jenssegers\Mongodb\MongodbServiceProvider::class,
+        Spatie\Fractal\FractalServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -226,6 +227,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'Fractal' => Spatie\Fractal\FractalFacade::class,
 
     ],
 
