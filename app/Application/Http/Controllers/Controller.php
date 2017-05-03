fb6e3e6909087cd7675ed738c65289e5f9e6ce4f
Jamie Burnip <jamieburnip@gmail.com>
2017-05-03 16:53:38 +0100

    Inject the command bus into the controllers

--- app/Application/Http/Controllers/Controller.php
+++ app/Application/Http/Controllers/Controller.php
@@ -2,12 +2,33 @@
 
 namespace Blog\Application\Http\Controllers;
 
-use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Foundation\Validation\ValidatesRequests;
+use Chief\CommandBus;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * Class Controller
+ *
+ * @package Blog\Application\Http\Controllers
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * @var CommandBus
+     */
+    protected $chief;
+
+    /**
+     * Controller constructor.
+     *
+     * @param CommandBus $chief
+     */
+    public function __construct(CommandBus $chief)
+    {
+        $this->chief = $chief;
+    }
 }
