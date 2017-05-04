4bbfcdf3e277192e42c917d0024fb99a41fff3f9
Jamie Burnip <jamieburnip@gmail.com>
2017-05-04 21:45:11 +0100

    Update all the routes

--- routes/web.php
+++ routes/web.php
@@ -13,18 +13,30 @@
 
 Auth::routes();
 
-Route::get('/', 'PagesController@index');
+Route::get('/', 'HomeController@index')->name('home.index');
 
-Route::group(['namespace' => 'Admin', 'middleware' => 'auth'], function () {
-    Route::get('/admin/', 'AdminPagesController@index');
-    Route::get('/admin/post/new', 'AdminPagesController@index');
-    Route::get('/admin/post/{id}/edit', 'AdminPagesController@index');
-    Route::get('/admin/post/{id}/delete', 'AdminPagesController@index');
+//Route::group(['namespace' => 'Admin', 'middleware' => 'auth'], function () {
+//    Route::get('/admin/', 'AdminPagesController@index');
+//    Route::get('/admin/post/new', 'AdminPagesController@new');
+//    Route::get('/admin/post/{id}/edit', 'AdminPagesController@edit');
+//    Route::get('/admin/post/{id}/delete', 'AdminPagesController@index');
+//
+//
+//    Route::post('/admin/post/store', 'PostApiController@store');
+//    Route::get('/admin/api/post/{postId}', 'AdminPagesController@apiGet');
+//});
 
-//    Route::get('/new-post', 'BlogController@create');
-//    Route::post('/new-post', 'BlogController@store');
-//    Route::delete('/post/{id}/delete', 'BlogController@delete')->name('post.delete');
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/new-post', 'PostsController@create')->name('post.create');
+    Route::post('/new-post', 'PostsController@store')->name('post.store');
+
+    Route::get('/{username}/{slug}/edit', 'PostsController@edit')->name('post.edit');
+    Route::post('/{username}/{slug}/edit', 'PostsController@update')->name('post.update');
+    Route::post('/{username}/{slug}/quick-publish', 'PostsController@publish')->name('post.quick-publish');
+
+    Route::delete('/post/delete', 'PostsController@destroy')->name('post.delete');
 });
 
-Route::get('/{username}/{slug}', 'BlogController@show');
-Route::get('/{username}', 'ProfileController@show');
+Route::get('/{username}/{slug}', 'ProfileController@show')->name('profile.show');
+Route::get('/{username}', 'ProfileController@index')->name('profile.index');
+
